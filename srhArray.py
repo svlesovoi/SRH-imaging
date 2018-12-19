6ac8a443eeab7913cdd8d48e11d2006afd0c1fe1
Sergey Lesovoi <svlesovoi@gmail.com>
2018-12-19 08:58:34 +0000

    headers

---
+++ srhArray.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 00:18:47 2018
+
+@author: Sergey
+"""
+
+class srhArray():
+    def __init__(self, fromFits = None):
+        if fromFits is None:
+            self.uvPlane
