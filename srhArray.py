eccd83404bd0c41ba845647643dc15acbb3d5d45
Sergey Lesovoi <svlesovoi@gmail.com>
2018-12-20 01:19:11 +0000

    uvSize added

--- srhArray.py
+++ srhArray.py
@@ -4,8 +4,11 @@ Created on Tue Dec 12 00:18:47 2018
 
 @author: Sergey
 """
+import numpy as NP
 
 class srhArray():
     def __init__(self, fromFits = None):
+	self.uvSize = 256
         if fromFits is None:
-            self.uvPlane
+            self.uvPlane = NP.zeros((self.uvSize, self.uvSize))
+            
---
+++ srhFits.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 00:18:47 2018
+
+@author: Sergey
+"""
